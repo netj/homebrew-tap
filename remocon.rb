5a8094e3ab9a14cbb61782afdfb6fe8ee0cbd468
Jaeho Shin <netj@sparcs.org>
2018-05-15 01:18:39 -0700

    remocon 0.2.0

--- remocon.rb
+++ remocon.rb
@@ -1,19 +1,42 @@
 class Remocon < Formula
-  desc "a simple tool for controlling remote host git repos"
+  desc "a tool for controlling remote host's clones of local git work trees"
   homepage "https://github.com/netj/remocon#readme"
   head "https://github.com/netj/remocon.git"
 
-  url "https://github.com/netj/remocon/archive/v0.1.0.tar.gz"
-  sha256 "eabfd550e2f6630a1ac0b26dc44cecfb2b8f2232cdd71639b85342c31658ea70"
-  version "0.1.0"
+  url "https://github.com/netj/remocon/archive/v0.2.0.tar.gz"
+  sha256 "d582c0fb2d691d99a29ac28048803dfcb11e4390c20f3654cc1684b6a3360408"
+  version "0.2.0"
 
   depends_on "rsync" => :optional
 
   def install
     bin.install "remocon"
+    doc.install "README.md"
   end
 
   test do
-    system "#{bin}/remocon", "help"
+    system "git", "clone", head.url
+    system "bash", "-xeuc", "
+    target=\"$PWD/target/remocon\"
+    export remote=\"localhost:${target%/remocon}\"
+    PATH=\"#{bin}:$PATH\"
+    cd remocon
+    remocon put
+
+    date >foo
+    git add foo
+    remocon put
+    diff -u {,\"$target\"/}foo
+
+    date >>README.md
+    remocon put
+    diff -u {,\"$target\"/}README.md
+
+    date >>\"$target\"/README.md
+    date  >\"$target\"/bar
+    remocon get
+    diff -u {,\"$target\"/}README.md
+    diff -u {,\"$target\"/}bar
+    "
   end
 end
