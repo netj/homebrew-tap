aa2fedd66e7977cc1e720513b8f4512a4b031e5b
Jaeho Shin <Jaeho.Shin@Gmail.com>
2026-04-05 20:59:07 -0700

    Update wtcode homepage to PyPI

--- wtcode.rb
+++ wtcode.rb
@@ -1,6 +1,6 @@
 class Wtcode < Formula
   desc "Effortlessly launch your favorite code tool in a git worktree"
-  homepage "https://github.com/netj/wtcode#readme"
+  homepage "https://pypi.org/project/wtcode/"
   head "https://github.com/netj/wtcode.git", branch: "main"
 
   url "https://github.com/netj/wtcode/archive/refs/tags/v0.1.3.tar.gz"
