aebfe534d47db769904f188d41f496af04bb4dcb
Jaeho Shin <Jaeho.Shin@Gmail.com>
2026-04-05 17:13:10 -0700

    Update wtcode formula to v0.1.2

--- wtcode.rb
+++ wtcode.rb
@@ -3,9 +3,9 @@ class Wtcode < Formula
   homepage "https://github.com/netj/wtcode#readme"
   head "https://github.com/netj/wtcode.git", branch: "main"
 
-  url "https://github.com/netj/wtcode/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "baf3104d5bcd0eadc1d028b86b61b4671ab1f2c412c79bc18df8971caa65cbd3"
-  version "0.1.0"
+  url "https://github.com/netj/wtcode/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "960689f370d999c8a938d108d0d44450334ff2c2c888d8a359fcf4c99d28b41f"
+  version "0.1.2"
   license "MIT"
 
   depends_on "fzf" => :recommended
