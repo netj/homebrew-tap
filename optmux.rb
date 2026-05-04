007325930f747519aec5fdf49e4f4d986405d1a5
Jaeho Shin <Jaeho.Shin@Gmail.com>
2026-05-04 09:14:11 -0700

    Update optmux to v0.3.0

--- optmux.rb
+++ optmux.rb
@@ -1,9 +1,9 @@
 class Optmux < Formula
   desc "Optimal, opinionated, batteries-included TMUX — recommended extras"
   homepage "https://pypi.org/project/optmux/"
-  url "https://github.com/netj/optmux/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "bb79adca6b1fa5b72fa8aa8ddc65697327b1fddd1011e008e3cd0fbcf9c17805"
-  version "0.2.1"
+  url "https://github.com/netj/optmux/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "d626a954e19fe17d63c300435613b4805f9aad8a18926a358ec442e257175d6e"
+  version "0.3.0"
   license "MIT"
 
   depends_on "uv"
