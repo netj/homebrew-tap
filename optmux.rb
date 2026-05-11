e3cd800f89b399a50b77775879e4aceb615e04c3
Jaeho Shin <netj@sparcs.org>
2026-05-11 00:09:37 -0700

    Update optmux to v0.4.0

--- optmux.rb
+++ optmux.rb
@@ -1,9 +1,9 @@
 class Optmux < Formula
   desc "Optimal, opinionated, batteries-included TMUX — recommended extras"
   homepage "https://pypi.org/project/optmux/"
-  url "https://github.com/netj/optmux/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "d626a954e19fe17d63c300435613b4805f9aad8a18926a358ec442e257175d6e"
-  version "0.3.0"
+  url "https://github.com/netj/optmux/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "8fc77567f0a54a1dd31d04c5cc288849fb433f129552e151ab7d6939b8a5229f"
+  version "0.4.0"
   license "MIT"
 
   depends_on "uv"
