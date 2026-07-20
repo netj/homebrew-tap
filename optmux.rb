91686c899a03e88aa0bdb89714b4e0e8a5d5cf3f
Jaeho Shin <netj@sparcs.org>
2026-07-20 00:06:30 -0700

    Update optmux to 0.6.0 (requires wtcode >= 0.2.0)

--- optmux.rb
+++ optmux.rb
@@ -1,9 +1,9 @@
 class Optmux < Formula
   desc "Optimal, opinionated, batteries-included TMUX — recommended extras"
   homepage "https://pypi.org/project/optmux/"
-  url "https://github.com/netj/optmux/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "73ed4154789235635fbb85e5c7278b4be18308e3fcecbc16dde5a943889139ee"
-  version "0.5.0"
+  url "https://github.com/netj/optmux/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "73ffe8e34bebfccaa6cd7bbdb070120822a7d02814092a92b393c4dd74835497"
+  version "0.6.0"
   license "MIT"
 
   depends_on "uv"
